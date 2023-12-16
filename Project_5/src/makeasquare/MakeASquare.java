8bf08ef62d72d1930c0551bfbdda09a72c971e1b
Amr Gamal <113727947+AmrrGamal@users.noreply.github.com>
2023-12-16 23:40:05 +0200

    Add files via upload

---
+++ Project_5/src/makeasquare/MakeASquare.java
@@ -0,0 +1,24 @@
+
+package makeasquare;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+
+public class MakeASquare {
+    
+    public static void main(String[] args)  {
+        openForm(new InputPieces());
+        
+    }
+
+    // open any form
+        public static void openForm(JFrame form) {
+            form.setLocationRelativeTo(null);
+            form.setDefaultCloseOperation(2);
+            form.pack();
+            form.setVisible(true);
+    }
+
+}
